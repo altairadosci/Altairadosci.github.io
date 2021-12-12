7a4eef341e864f1346f4edc2230177296d635ee0
altairadosci <50335707+altairadosci@users.noreply.github.com>
2021-12-12 16:47:28 +0300

    Add files via upload

---
+++ My_first_site/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Snaken</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="header-wrap">
+	<header>
+		<div class="logo-wrapper">
+			<div class="logo">
+				<div class="ring"></div>
+				<div class="ring"></div>
+				<div class="ring"></div>
+			</div>
+		</div>
+		<div class="navigation">
+			<a href="#">
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				Home
+			</a>
+			<a href="#">
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				News
+			</a>
+			<a href="#">
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				Game
+			</a>
+			<a href="#">
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				Message
+			</a>
+		</div>
+	</header>
+	</div>
+	<section>
+		<div class="container">	
+			<div class="description">
+				<div class="avatar">
+					<img src="Image/avatar.jpg" alt="avatar">
+				</div>
+				<div class="name">
+					<p>Eremeev Roman</p>
+				</div>
+				<div class="info">
+					<div class="info-item">
+						Status: <span>test status</span>
+					</div>
+					<div class="info-item">
+						Born: <span>test date</span>
+					</div>
+					<div class="info-item">
+					City: <span>test city</span>
+					</div>
+				</div>
+				<div class="btn" id="change">
+					<a href="#">Change</a>
+				</div>
+			</div>
+			<div class="posts-block">
+				<div class="posts-wrap">
+					<div class="head-posts">
+						<h1>My life</h1>
+					</div>
+					<div class="new-post">
+						<form action="">
+							<input type="text" placeholder="Input your new post">
+							<div class="btn">
+								<a href="#">Save</a>
+							</div>
+						</form>
+					</div>
+					<div class="post">
+						
+					</div>
+				</div>
+			</div>	
+		</div>
+	</section>
+</body>
+</html>
